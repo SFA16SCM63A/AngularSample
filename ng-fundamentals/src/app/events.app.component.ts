69734b9565e402f1ea73505fa980b39dde2cb545
sachin <sambalka@hawk.iit.edu>
2019-06-01 16:39:47 -0400

    added event-list-component

--- ng-fundamentals/src/app/events.app.component.ts
+++ ng-fundamentals/src/app/events.app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
-  template: `<h1>Hello there<h1>
-  <img src="/assets/images/basic-shield.png">
-  `,
+  template: `<events-list></events-list>`
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
