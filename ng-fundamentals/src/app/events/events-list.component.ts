10120ee57de9d294361d28b0b8f257ee324df7aa
sachin <sambalka@hawk.iit.edu>
2019-06-01 17:17:43 -0400

    added thumnail component

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -3,30 +3,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    template:`
-        <div>
+    template:`<div>
             <h1>Upcoming Angular Events</h1>
             <hr/>
-            <div class="well hoverwell thumbnail">
-                <h2>{{event.name}}</h2>
-                <div>Date: {{event.data}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.address}}</span>
-                    <span>&nbsp;</span>
-                    <span>{{event.location.city}},
-                            {{event.location.country}}</span>
-                </div>
-            </div>
-            
-
+            <event-thumbnail 
+            (eventClick)="handleEventClicked($event)"
+            [event]="event1"></event-thumbnail>
         </div>
     `
 })
 export class EventsListComponent{
 
-    event={
+    event1={
         id: 1,
         name: 'Angular Connect',
         data: '9/26/2036',
@@ -38,6 +26,10 @@ export class EventsListComponent{
             city: 'London',
             country: 'England'
         }
+    }
+
+    handleEventClicked(data){
+            console.log("Received: "+data)
     }
 
 }
