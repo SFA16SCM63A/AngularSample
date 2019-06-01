6c7f0e1e79ecbc7889dc0ac15bb549eb7f9eaa79
sachin <sambalka@hawk.iit.edu>
2019-06-01 19:00:02 -0400

    added changes if event is null. Added ? opertaor to aboid crashing app when value is null

--- ng-fundamentals/src/app/events/event-thumbnail.components.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.components.ts
@@ -3,18 +3,20 @@ import {Component,Input, Output, EventEmitter} from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template:`  <div class="well hoverwell thumbnail">
-                        <h2>{{event.name}}</h2>
-                        <div>Date: {{event.data}}</div>
-                        <div>Time: {{event.time}}</div>
-                        <div>Price: \${{event.price}}</div>
+                        <h2>{{event?.name}}</h2>
+                        <div>Date: {{event?.data}}</div>
+                        <div>Time: {{event?.time}}</div>
+                        <div>Price: \${{event?.price}}</div>
                         <div>
-                            <span>Location: {{event.location.address}}</span>
-                            <span class="pad-left">{{event.location.city}},
-                                    {{event.location.country}}</span>
+                            <span>Location: {{event?.location.address}}</span>
+                            <span class="pad-left">{{event?.location.city}},
+                                    {{event?.location.country}}</span>
                         </div>
                         
                 </div>`,
-        styles:[`.pad-left { margin-left: 10px; }
+        styles:[`
+                .thumbnail {min-height: 210px;}
+                .pad-left { margin-left: 10px; }
                 .well div { color: #bbb; }
         `]
 })
