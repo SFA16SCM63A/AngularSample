05ba5bd4513b6d6aa0190ccd48c4b2a997053b2f
sachin <sambalka@hawk.iit.edu>
2019-06-02 01:33:53 -0400

    added 'Create new event' component

--- ng-fundamentals/src/routes.ts
+++ ng-fundamentals/src/routes.ts
@@ -1,11 +1,13 @@
 import {Routes} from '@angular/router'
 import { EventsListComponent } from './app/events/events-list.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
+import { CreateEventComponent } from './app/events/create-event.component';
 
 export const appRoutes:Routes = [
-
+    {path:'event/new',component:CreateEventComponent},
     { path:'events',component: EventsListComponent},
     { path:'events/:id',component: EventDetailsComponent},
     { path:'',redirectTo:'/events',pathMatch:'full'}
+    
     
 ]
